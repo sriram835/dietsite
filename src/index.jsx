57c718f17354791d614ed526f6cf9d1984d21270
Sriram.T <sriramtatikonda9@gmail.com>
2023-01-08 05:33:31 +0530

    rendered feed properly

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css' 
+
+class Feed extends Component {
+    render() { 
+        return (
+            <div>
+                <h3>Feedback!</h3>
+            </div>
+        );
+    }
+}
+ 
+export default Feed;
