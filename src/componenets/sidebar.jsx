c919ecc07ff4d635819ea9b5e9b9335c2fd16f88
Sriram.T <sriramtatikonda9@gmail.com>
2023-01-13 10:57:05 +0530

    successfully completed responsive sidebar

--- src/componenets/sidebar.jsx
+++ src/componenets/sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import "../style/sidebar.css";
+import "./style/sidebar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
@@ -8,66 +8,46 @@ import {
   faBowlFood,
   faDumbbell,
   faMagnifyingGlass,
+  faGear,
 } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-
-class SideBar extends Component {
-  render() {
-    return (
-      <div>
-        <h1 className="testdiv" id="testDiv" >
-          Hi!
-        </h1>
-        <nav className="sbnav">
-          <ul className="sbul">
-            <li className="sbli">
-              <a href="" className="sbamenu" >
-                <FontAwesomeIcon icon={faBars} className="sblogo" />
-                <span className="sbtext">Sidebar</span>
-              </a>
-            </li>
-            <li className="sbli">
-              <a href="" className="sbsa">
-                <FontAwesomeIcon
-                  icon={faMagnifyingGlass}
-                  className="sblogo"
-                ></FontAwesomeIcon>
-              </a>
-              <input type="text" placeholder="search" className="sbinput" />
-            </li>
-            <li className="sbli">
-              <a href="" className="sba">
-                <FontAwesomeIcon
-                  icon={faHouse}
-                  className="sblogo"
-                ></FontAwesomeIcon>
-                <span className="sbtext">Home</span>
-              </a>
-            </li>
-            <li className="sbli">
-              <a href="" className="sba">
-                <FontAwesomeIcon
-                  icon={faDumbbell}
-                  className="sblogo"
-                ></FontAwesomeIcon>
-                <span className="sbtext" style={{ paddingLeft: "37px" }}>
-                  Exercises
-                </span>
-              </a>
-            </li>
-            <li className="sbli">
-              <a href="" className="sba">
-                <FontAwesomeIcon icon={faBowlFood} className="sblogo" />
-                <span className="sbtext" style={{ paddingLeft: "42px" }}>
-                  Recipes
-                </span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+const SideBar = () => {
+  const [isOpen, setIsopen] = useState(false);
+  const toggle = () => setIsopen(!isOpen);
+  return (
+    <div className="container" style={{width: isOpen ? "240px" : "45px"}}>
+      <div className="opbtn" onClick={toggle} style={{width: isOpen ? "240px" : "45px"}}>
+        <FontAwesomeIcon icon={faBars} className="oplg" />
+        <span className="optx" style={{display: isOpen ? "block" : "none"}}>SideBar</span>
       </div>
-    );
-  }
-}
+
+      <div className="hmbtn" style={{width: isOpen ? "240px" : "45px" , opacity: isOpen ? "0.5" : "1"}}>
+        <FontAwesomeIcon icon={faHouse} className="hmlg" />
+        <span className="hmtx" style={{display: isOpen ? "block" : "none"}}>Home</span>
+      </div>
+
+      <div className="srch" style={{width: isOpen ? "240px" : "45px"}}>
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="srchlg" style={{opacity: isOpen ? "0.5" : "1"}}/>
+        <input className="srchinpt" placeholder="Search" style={{display: isOpen ? "block" : "none"}} />
+      </div>
+
+      <div className="exerbtn" style={{width: isOpen ? "240px" : "45px", opacity: isOpen ? "0.5" : "1"}}>
+        <FontAwesomeIcon icon={faDumbbell} className="exerlg" />
+        <span className="exertx" style={{display: isOpen ? "block" : "none"}}>Exercises</span>
+      </div>
+
+      <div className="rpsbtn" style={{width: isOpen ? "240px" : "45px", opacity: isOpen ? "0.5" : "1"}}>
+        <FontAwesomeIcon icon={faBowlFood} className="rpslg" />
+        <span className="rpstx" style={{display: isOpen ? "block" : "none"}}>Recipes</span>
+      </div>
+
+      <div className="stgbtn" style={{width: isOpen ? "240px" : "45px", opacity: isOpen ? "0.5" : "1"}}>
+        <FontAwesomeIcon icon={faGear} className="stglg" />
+        <span className="stgtx" style={{display: isOpen ? "block" : "none"}}>Settings</span>
+      </div>
+    </div>
+  );
+};
 
 export default SideBar;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import Feed from "./componenets/feedback.jsx";
 import SideBar from "./componenets/sidebar.jsx";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<SideBar />);
+
+
